adcfd051f6215e62184ff68f336714576e95b7b3
camilabersan <camilabersan.564@gmail.com>
2023-11-27 10:06:48 -0300

    feat: changes

--- src/controllers/person.controllers.js
+++ src/controllers/person.controllers.js
@@ -12,3 +12,32 @@ export const getPerson = (req, res) =>{
         return res.status(200).send({message: "Cadastrado com sucesso"});
     }
 }
+
+export const createPerson = (req, res) => {
+    const {name, birthDate, email, instagram, position, description} = req.body;
+    const error = [];
+
+    if(!name || !birthDate || !email || !instagram || !position || !description){
+        error.push("Preencha todos os campos");
+    }
+
+    if(name.length < 3){
+        error.push("Nome inválido, digite com 3 ou mais caracteres")
+    }
+
+    if(description.length > 150){
+        error.push("Digite com menos de 150 caracteres")
+    }
+
+    if(error.length > 0){
+        res.status(400).send(error);
+        return
+    }
+
+    const person = new Person(name, birthDate, email, instagram, position, description)
+
+    list.addPersons(person);
+
+    return res.status(200).send({message: "Cadastrado com sucesso", person});
+
+}
