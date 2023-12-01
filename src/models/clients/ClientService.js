70410d18f970cc075200b6c61265a5adddf7ee85
Rafael Custodio Cumpri <rafaelcumpri.senai564@gmail.com>
2023-12-01 07:53:35 -0300

    Add client validation and age calculation methods

--- src/models/clients/ClientService.js
+++ src/models/clients/ClientService.js
@@ -45,6 +45,58 @@ class ClientService {
         let client = this.clients.find(client => client.email === email);
         return client;
     }
+
+    validateClient(client) {
+        let errors = [];
+
+        if (!client.name || !client.birthDate || !client.email || !client.password || !client.money || !client.cpf || !client.cep) {
+            errors.push("Preencha todos os campos");
+        }
+
+        if (client.name.length < 3) {
+            errors.push("Nome inválido, digite com 3 ou mais caracteres");
+        }
+
+        if (client.password.length < 6) {
+            errors.push("Senha inválida, digite com 6 ou mais caracteres");
+        }
+
+        if (client.cpf.length !== 11) {
+            errors.push("CPF inválido, digite com 11 caracteres");
+        }
+
+        if (client.cep.length !== 8) {
+            errors.push("CEP inválido, digite com 8 caracteres");
+        }
+
+        let age = this.calculateAge(client.birthDate);
+
+        if (age < 18) {
+            errors.push("Cliente menor de idade");
+        }
+
+        if (age > 120) {
+            errors.push("Cliente muito velho");
+        }
+
+        return errors;
+    }
+
+    calculateAge(birthDate) {
+        let today = new Date();
+        let birthDateArray = birthDate.split("-");
+        let birthYear = birthDateArray[0];
+        let birthMonth = birthDateArray[1];
+        let birthDay = birthDateArray[2];
+        let age = today.getFullYear() - birthYear;
+        let month = today.getMonth() + 1;
+
+        if (month < birthMonth || (month === birthMonth && today.getDate() < birthDay)) {
+            age--;
+        }
+
+        return age;
+    }
 }
 
 export default ClientService;
