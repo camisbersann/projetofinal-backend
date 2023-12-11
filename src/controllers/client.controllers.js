06d7368aa7e123e9cb590a58e025b15e4b77849d
Rafael Custodio Cumpri <rafaelcumpri.senai564@gmail.com>
2023-12-11 14:37:22 -0300

    Add client validation functions

--- src/controllers/client.controllers.js
+++ src/controllers/client.controllers.js
@@ -27,11 +27,100 @@ export const getClientById = (req, res) => {
 export const createClient = (req, res) => {
     const { name, birthdate, email, password, money, cpf, cep } = req.body;
 
+    const today = new Date();
+    const birthDateObj = new Date(birthdate);
+    const age = today.getFullYear() - birthDateObj.getFullYear();
+
+    if (age < 18) {
+        return res.status(400).send({ message: "Clientes devem ter pelo menos 18 anos." });
+    }
+
+    if (name.length < 3) {
+        return res.status(400).send({ message: "O nome deve ter no mínimo 3 letras." });
+    }
+
+    if (!validateCPF(cpf)) {
+        return res.status(400).send({ message: "CPF inválido." });
+    }
+
+    if (!validateCEP(cep)) {
+        return res.status(400).send({ message: "CEP inválido." });
+    }
+
     let client = new ClientPerson(name, birthdate, email, password, money, cpf, cep);
     clientService.addClient(client);
 
     return res.status(200).send({ message: "Cadastrado com sucesso", client });
 }
+
+const validateCEP = (cep) => {
+    // Remove non-numeric characters
+    cep = cep.replace(/\D/g, '');
+
+    // Check if the CEP has 8 digits
+    if (cep.length !== 8) {
+        return false;
+    }
+
+    // Check if all digits are the same
+    if (/^(\d)\1{7}$/.test(cep)) {
+        return false;
+    }
+
+    return true;
+}
+
+const validateCPF = (cpf) => {
+    // Remove non-numeric characters
+    cpf = cpf.replace(/\D/g, '');
+
+    // Check if the CPF has 11 digits
+    if (cpf.length !== 11) {
+        return false;
+    }
+
+    // Check if all digits are the same
+    if (/^(\d)\1{10}$/.test(cpf)) {
+        return false;
+    }
+
+    // Validate CPF algorithm
+    let sum = 0;
+    let remainder;
+
+    for (let i = 1; i <= 9; i++) {
+        sum += parseInt(cpf.substring(i - 1, i)) * (11 - i);
+    }
+
+    remainder = (sum * 10) % 11;
+
+    if ((remainder === 10) || (remainder === 11)) {
+        remainder = 0;
+    }
+
+    if (remainder !== parseInt(cpf.substring(9, 10))) {
+        return false;
+    }
+
+    sum = 0;
+
+    for (let i = 1; i <= 10; i++) {
+        sum += parseInt(cpf.substring(i - 1, i)) * (12 - i);
+    }
+
+    remainder = (sum * 10) % 11;
+
+    if ((remainder === 10) || (remainder === 11)) {
+        remainder = 0;
+    }
+
+    if (remainder !== parseInt(cpf.substring(10, 11))) {
+        return false;
+    }
+
+    return true;
+};
+
 
 export const deleteClientById = (req, res) => {
     const { id } = req.params;
